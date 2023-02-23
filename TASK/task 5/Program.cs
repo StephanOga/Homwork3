289c4b94292a914e7b9e32e090dd2cfe0888fcb9
Stepan <sud_pravo@bk.ru>
2023-02-23 22:13:15 +0700

    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

--- TASK/task 5/Program.cs
+++ TASK/task 5/Program.cs
@@ -3,35 +3,8 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 
-double Degree(int number, int c)
-{
-    double numberDegree = 1;
+*/ 
 
-    for (int i = 0; i < c; i++)
-    {
-        numberDegree *= number;
-    }
-    return numberDegree;
-}
-string TabCub(int n)
-{
-    double cub = 1;
-    string result = "";
-    for (double count = 1; count <= n; count++)
-    {
-        cub = Degree(count, 3);
-        result = result + cub + ",";
-    }
-    return result;
-}
-
-
-Console.WriteLine("вВЕДИТЕ ЛЮБОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО ");
-int N = int.Parse(Console.Readline());
-string Cub = TabCub(N);
-Console.WriteLine(Cub);
-
-*/
 double Degree(int number, int c)
 {
     double numberDegree = 1;
